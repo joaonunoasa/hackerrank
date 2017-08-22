b94d405f6afd534f4bc8f5076512225bed5f1707
joaosa <joao.sa@iconsolutions.com>
2017-08-22 14:58:53 +0100

    inject properties on type

--- spring-tutorial/src/main/java/org/javabrains/Triangle.java
+++ spring-tutorial/src/main/java/org/javabrains/Triangle.java
@@ -2,7 +2,17 @@ package org.javabrains;
 
 public class Triangle {
 
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     void draw() {
-        System.out.println("Triangle draw");
+        System.out.println("Triangle draw of type " + getType());
     }
 }
