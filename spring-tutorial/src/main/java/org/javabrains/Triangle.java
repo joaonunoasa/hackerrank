5c88c4935d85321735e4377db06100d149a42a4a
joaosa <joao.sa@iconsolutions.com>
2017-08-26 18:33:23 +0100

    list of points

--- spring-tutorial/src/main/java/org/javabrains/Triangle.java
+++ spring-tutorial/src/main/java/org/javabrains/Triangle.java
@@ -1,36 +1,22 @@
 package org.javabrains;
 
+import java.util.List;
+
 public class Triangle {
 
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    private List<Point> points;
 
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
     void draw() {
-        System.out.println("Triangle Points: " + getPointA() + " " + getPointB() + " " + getPointC());
+        for (Point point : points) {
+            System.out.println("Triangle Point: " + point);
+        }
     }
 }
