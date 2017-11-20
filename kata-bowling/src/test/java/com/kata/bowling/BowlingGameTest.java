55ba74a03d28ec00b29b7c172a5df48f892f4fdf
Joao Sa <joao.sa@iconsolutions.com>
2017-11-20 20:53:50 +0000

    bowling game

---
+++ kata-bowling/src/test/java/com/kata/bowling/BowlingGameTest.java
@@ -0,0 +1,15 @@
+package com.kata.bowling;
+
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+    @Test
+    public void gameScoreShouldBeZeroWhenScoringZeroInAll() {
+
+        BowlingGame bowlingGame = new BowlingGame();
+
+        
+
+    }
+}
